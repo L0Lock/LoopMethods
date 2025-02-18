6d32fc3a18914cb68873daab1c18823d216d41cf
Loïc 'Lauloque' Dautry <loic.dautry@gmail.com>
2025-02-17 21:10:15 -0500

    first functional implementation of icons - needs better icons

--- loop_methods/__init__.py
+++ loop_methods/__init__.py
@@ -1,4 +1,6 @@
 import bpy
+import os
+from bpy.utils import previews
 
 bl_info = {
     "name": "Loop Methods",
@@ -14,14 +16,66 @@ bl_info = {
     "support": "COMMUNITY",
 }
 
+icon_collections = {}
+
+def load_icons():
+    global icon_collections
+    pcoll = previews.new()
+    icons_path = os.path.join(os.path.dirname(__file__), "icons")
+
+    pcoll.load("PBL_Loop", os.path.join(icons_path, "Loop.png"), 'IMAGE')
+    pcoll.load("PBL_ping_pong", os.path.join(icons_path, "PinPong.png"), 'IMAGE')
+    pcoll.load("PBL_restore", os.path.join(icons_path, "Revert.png"), 'IMAGE')
+    pcoll.load("PBL_start", os.path.join(icons_path, "Start.png"), 'IMAGE')
+    pcoll.load("PBL_stop", os.path.join(icons_path, "Stop.png"), 'IMAGE')
+
+    icon_collections["main"] = pcoll
+
+def unload_icons():
+    for pcoll in icon_collections.values():
+        previews.remove(pcoll)
+    icon_collections.clear()
+
 # Enum for the different playback modes
 def get_playback_modes(self, context):
+    icons = icon_collections.get("main", {})
+    
     return [
-        ('Loop', "Loop (default)", "Standard looping playback (default)"),
-        ('PBL_stop', "Play Once", "Play once and stop playback."),
-        ('PBL_restore', "Play Once & Restore", "Play once and jump back to where the playback started."),
-        ('PBL_start', "Play Once & Jump Start", "Play once and jump to scene Start frame."),
-        ('PBL_ping_pong', "Ping-Pong", "Loop back and forth between start and end")
+        (
+            'Loop',
+            "Loop (default)",
+            "Standard looping playback (default)",
+            icons.get("PBL_Loop").icon_id if "PBL_Loop" in icons else "",
+            0
+        ),
+        (
+            'PBL_stop',
+            "Play Once",
+            "Play once and stop playback.",
+            icons.get("PBL_stop").icon_id if "PBL_stop" in icons else "",
+            1
+        ),
+        (
+            'PBL_restore',
+            "Play Once & Restore",
+            "Play once and jump back to where the playback started.",
+            icons.get("PBL_restore").icon_id if "PBL_restore" in icons else "",
+            2
+        ),
+        (
+            'PBL_start',
+            "Play Once & Jump Start",
+            "Play once and jump to scene Start frame.",
+            icons.get("PBL_start").icon_id if "PBL_start" in icons else "",
+            3
+        ),
+        (
+            'PBL_ping_pong',
+            "Ping-Pong",
+            "Loop back and forth between start and end",
+            icons.get("PBL_ping_pong").icon_id if "PBL_ping_pong" in icons else "",
+            4
+        ),
     ]
 
 def PBL_stop(scene):
@@ -71,6 +125,7 @@ def draw_playback_mode_dropdown(self, context):
         row.prop(scene.pbl_settings, "playback_mode", text="")
 
 def register():
+    load_icons()
     bpy.utils.register_class(PBL_Settings)
     bpy.types.Scene.pbl_settings = bpy.props.PointerProperty(type=PBL_Settings)
 
@@ -78,6 +133,7 @@ def register():
     bpy.types.DOPESHEET_HT_header.append(draw_playback_mode_dropdown)
 
 def unregister():
+    unload_icons()
     bpy.utils.unregister_class(PBL_Settings)
     del bpy.types.Scene.pbl_settings
 
